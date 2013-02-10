c39986e514123df4c4743e00483d3aa60ed35b61
Jed Prentice <jedprentice@gmail.com>
2013-02-09 22:06:14 -0500

    Results of rails new

---
+++ order-manager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderManager::Application.config.secret_token = '9b526b593d12ca2db3cf95b48ed49490401f4f4c6abf18699632f2f5c8b0aa7e837593d2643ccbc07a0c5f8b951e64866acdd1e4c27310bb964a0ac6e91a6d9a'
