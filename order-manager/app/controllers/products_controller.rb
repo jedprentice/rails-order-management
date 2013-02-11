8dfe98508484b191ab04eec3c7bece7e51e4ff33
Jed Prentice <jedprentice@gmail.com>
2013-02-10 21:46:42 -0500

    Implemented product index and show actions

--- order-manager/app/controllers/products_controller.rb
+++ order-manager/app/controllers/products_controller.rb
@@ -1,2 +1,9 @@
 class ProductsController < ApplicationController
+  def index
+    render :json => Product.find(:all)
+  end
+
+  def show
+    render :json => Product.find(params[:id])
+  end
 end
--- order-manager/test/functional/products_controller_test.rb
+++ order-manager/test/functional/products_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
+require 'json'
 
 class ProductsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'index' do
+    get :index
+    assert_response :success
+    objects = parse_response
+    assert_equal 2, objects.length, 'Wrong number of objects'
+  end
+
+  test 'show' do
+    expected = products(:one)
+    get :show, {:id => expected.id}
+    assert_response :success
+
+    object = parse_response
+    assert_equal expected.name, object['name'], 'Wrong name'
+    assert_equal expected.price.to_s, object['price'], 'Wrong price'
+  end
+
+  private
+
+  def parse_response
+    JSON.parse(@controller.response.body)
+  end
 end
